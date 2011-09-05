8ce5a3a622a03ec808cf006e372c8a69c6a43bc7
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-09-05 00:03:07 -0300

    list has many tasks

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ActiveRecord::Base
+  has_many :tasks
   belongs_to :user
   validates_presence_of :name
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe List do
   it { should validate_presence_of :name }
   it { should belong_to :user }
+  it { should have_many :tasks }
 end

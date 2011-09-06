66ca91d5e939db1bbf3f89dbd6c3918e88190f3c
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-09-06 01:38:35 -0300

    a list must accept nested attributes for tasks

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,6 @@ class List < ActiveRecord::Base
   has_many :tasks
   belongs_to :user
   validates_presence_of :name
+
+  accepts_nested_attributes_for :tasks, :allow_destroy => true
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -4,4 +4,12 @@ describe List do
   it { should validate_presence_of :name }
   it { should belong_to :user }
   it { should have_many :tasks }
+
+  describe "nested attributes" do
+    it "accepts nested attributes for tasks" do
+      list = List.new(:name => 'list', :user => Factory(:user), :private => true, :tasks_attributes => { "0" => { :description => "Task1" } })
+      list.save
+      list.tasks.first.description == 'Task1'
+    end
+  end
 end

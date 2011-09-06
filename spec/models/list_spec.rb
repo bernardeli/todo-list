81e54f669ded715ff9867d2e851678d6a7901135
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-09-06 01:45:31 -0300

    list should reject those tasks with no description

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -6,10 +6,20 @@ describe List do
   it { should have_many :tasks }
 
   describe "nested attributes" do
-    it "accepts nested attributes for tasks" do
-      list = List.new(:name => 'list', :user => Factory(:user), :private => true, :tasks_attributes => { "0" => { :description => "Task1" } })
-      list.save
-      list.tasks.first.description == 'Task1'
+    context "when parameters are filled in" do
+      it "accepts nested attributes for tasks" do
+        list = List.new(:name => 'list', :user => Factory(:user), :private => true, :tasks_attributes => { "0" => { :description => "Task1" } })
+        list.save
+        list.tasks.first.description == 'Task1'
+      end
+    end
+
+    context "when parameters are not filled in" do
+      it "rejects nested attributes for tasks" do
+        list = List.new(:name => 'list', :user => Factory(:user), :private => true, :tasks_attributes => { "0" => { :description => "" } })
+        list.save
+        list.tasks.should be_empty
+      end
     end
   end
 end

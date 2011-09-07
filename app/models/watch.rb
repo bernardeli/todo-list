1ea35825846296ddd8890f95ec53e37a30cc6850
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-09-07 02:44:05 -0300

    added association to watch

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,3 +1,4 @@
 class Watch < ActiveRecord::Base
   belongs_to :list
+  belongs_to :user
 end
--- spec/models/watch_spec.rb
+++ spec/models/watch_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Watch do
   it { should belong_to :list }
+  it { should belong_to :user }
 end

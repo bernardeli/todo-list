a229114fc9806db0ae853360db06f0f045ca617b
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-09-07 23:43:47 -0300

    changed redirection after creating a new watch

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -3,6 +3,10 @@ class WatchesController < InheritedResources::Base
   actions :index, :create, :destroy
   respond_to :html, :json
 
+  def create
+    create! { user_list_path(resource.list.user.id, resource.list_id) }
+  end
+
   protected
 
   def begin_of_association_chain
--- spec/controllers/watches_controller_spec.rb
+++ spec/controllers/watches_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe WatchesController do
+  let(:user) { Factory(:user) }
+  before(:each) { sign_in user }
 
+  context "POST create" do
+    it "redirects to user list path" do
+      list = Factory(:list, :user => user)
+      post :create, :watch => { :user_id => user.id, :list_id => list.id }
+      response.should redirect_to user_list_path(user.id, list.id)
+    end
+  end
 end

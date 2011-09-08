8012568243d92b499ebc443590c7045c92e60c98
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-09-07 22:14:32 -0300

    doesn't need to include current_user at overview#index

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -2,6 +2,6 @@ class OverviewController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @users = User.lasts_signed_in.limit(10)
+    @users = User.lasts_signed_in.limit(10).where("id <> ?", current_user.id)
   end
 end

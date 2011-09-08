720e1bf10ef2225f87bd0e66016148c024ba3b13
Ricardo Bernardelli <ricardobcs@gmail.com>
2011-09-07 22:13:38 -0300

    a little more of title

--- app/helpers/users/lists_helper.rb
+++ app/helpers/users/lists_helper.rb
@@ -1,9 +1,9 @@
 module Users::ListsHelper
   def link_to_watch(list)
     if current_user.watch?(list)
-      link_to "Unwatch", watch_path(current_user.watch(list)), :method => :delete
+      link_to "Unwatch", watch_path(current_user.watch(list)), :method => :delete, :title => "Unwatch"
     else
-      link_to "Watch", watches_path(:watch => { :list_id => list.id }), :method => :post
+      link_to "Watch", watches_path(:watch => { :list_id => list.id }), :method => :post, :title => "Watch"
     end
   end
 end
